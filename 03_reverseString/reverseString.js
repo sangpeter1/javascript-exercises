8df874bbeaa955bf2fe341f6b0ba65fe817f2e61
sangpeter1 <sang.peterchanmin@gmail.com>
2022-10-07 22:12:55 -0500

    Complete reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let string = '';
+for (i = word.length - 1; i >= 0; i--) {
+    string += word.slice (i, i+1);
+}
+return string
 };
 
 // Do not edit below this line
