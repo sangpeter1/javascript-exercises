9add255028b035c01041d3f5ea0775d30423f321
sangpeter1 <sang.peterchanmin@gmail.com>
2022-10-09 14:05:56 -0500

    Complete 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    if (x%4 === 0 && x%100 !== 0 || x%400 ===0)
+    return true;
+    else return false;
 };
-
+const x = leapYears(2004)
+console.log (x)
 // Do not edit below this line
 module.exports = leapYears;
