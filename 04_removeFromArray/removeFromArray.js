117ae1d1ad0a972f81a10dec7c4559403ff55566
sangpeter1 <sang.peterchanmin@gmail.com>
2022-10-09 13:38:40 -0500

    Complete 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...otherArg) {
+    let newArray = [];
+    for (let i = 0; i < list.length; i++) {
+        if (otherArg.indexOf(list[i]) === -1) 
+        newArray.push(list[i]);
+    
+    }
+    return newArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+let result = 0;
+if (a< 0 || b < 0 || isNaN(a)|| typeof a !== "number" || typeof b !== "number")
+    result = "ERROR";
 
+else if (a < b) {
+    for (let i = a; i <= b; i++) 
+    result += i;
+}
+else if (b < a) {
+    for (let i = b; i <= a; i++) 
+    result += i;
+}
+return result 
 };
+
+const x = sumAll ('8',1);
+console.log (x)
 
 // Do not edit below this line
 module.exports = sumAll;
