fadf9c5d3eefb94d50e7d1c6495e9d0f747171f2
sangpeter1 <sang.peterchanmin@gmail.com>
2022-10-09 20:12:45 -0500

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32)* 5/9
+  return Math.round(c * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * 9/5) + 32
+  return Math.round(f * 10) / 10
 };
+
+ftoc(32)
+ctof(38)
 
 // Do not edit below this line
 module.exports = {
